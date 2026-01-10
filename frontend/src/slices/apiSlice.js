c5292527e7ec5182b42828c211ca29d7d0980632
aymane <atbohos@gmail.com>
2026-01-10 20:24:04 +0100

    nod nssali ncha allah

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { BASE_URL } from '../constants'
-import { User } from 'lucide-react'
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' })
 

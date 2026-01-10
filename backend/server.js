e77932610d1aa57669359a6b4770ecc9b7dfe90d
aymane <atbohos@gmail.com>
2026-01-10 21:12:19 +0100

    akhiran

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(cookieParser())
 // CORS Logic
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production' ? false : 'http://localhost:5173',
+    origin: ['https://unccodestore.netlify.app', 'http://localhost:5173'],
     credentials: true,
   })
 )
